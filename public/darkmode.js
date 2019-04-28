f17cd4609a47c07a23822264af5a43ba81f74c81
Aman Raj <archanaamanraj@gmail.com>
2019-04-28 15:33:06 +0530

    Light mode button 🔳
    Minor fixes in index.html and removed log from js.

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,11 +1,9 @@
 $('#toggleTheme').on('change', () => {
-    if ($('#toggleTheme').is(":checked")) {
-        $('body').removeClass('uk-section-secondary')
-        $('body').addClass('uk-section-defaultd')
-        console.log('>>> [darkmode.js:6] light ')
-    } else {
-        $('body').removeClass('uk-section-defaultd')
-        $('body').addClass('uk-section-secondary')
-        console.log('>>> [darkmode.js:10] dark ')
-    }
+  if ($('#toggleTheme').is(':checked')) {
+    $('body').removeClass('uk-section-secondary');
+    $('body').addClass('uk-section-defaultd');
+  } else {
+    $('body').removeClass('uk-section-defaultd');
+    $('body').addClass('uk-section-secondary');
+  }
 });
