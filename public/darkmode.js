831c5fe3fc5fb05ee22ffbc6dba7f0ba426c10cd
Prathmesh Khandelwal <prathmeshkhandelwal83@gmail.com>
2020-08-30 15:20:52 +0530

    commented code removed

--- public/darkmode.js
+++ public/darkmode.js
@@ -7,14 +7,3 @@ $('#toggleTheme').on('change', () => {
     $('body').addClass('uk-section-secondary')
   }
 })
-
-// const toggleTheme = document.getElementById("toggleTheme")
-// const input = document.getElementById('email')
-
-// toggleTheme.addEventListener('click',()=>{
-//   if(toggleTheme.checked){
-//     input.style.backgroundColor = "white"
-//   }else{
-//     input.style.background = "black"
-//   }
-// })
