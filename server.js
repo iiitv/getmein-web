eb67cc5f2161f420e81b3584cbb08d7bf4a6d156
Ajay Jain <ajayjain1024@gmail.com>
2024-09-29 20:10:44 +0530

    Set server to use dynamic port

--- server.js
+++ server.js
@@ -138,6 +138,6 @@ const addMember = data => {
   return promise
 }
 
-const listener = app.listen(3000 || process.env.PORT, () => {
+const listener = app.listen(process.env.PORT||3000, () => {
   console.log(`Your app is listening on port ${listener.address().port}`)
 })
